48ac0ab3a49b7a76e5f801e944775a6029c346b2
jiali <mahyarerad@gmail.com>
2025-02-16 18:30:15 +0330

    Adding raw Html header

--- header.php
+++ header.php
@@ -6,3 +6,33 @@ if (!defined('ABSPATH')) exit;
 wp_head(); 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="fa">
+    <head>
+		<meta charset="utf-8" />
+		<meta name="author" content="" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+
+        <title>LearnUp - قالب HTML دوره آنلاین و آموزش</title>
+
+        <!-- Custom CSS -->
+        <link href="assets/css/styles.css" rel="stylesheet">
+
+		<!-- Custom Color Option -->
+		<link href="assets/css/colors.css" rel="stylesheet">
+
+    </head>
+
+    <body class="red-skin rtl">
+	
+        <!-- ============================================================== -->
+        <!-- Preloader - style you can find in spinners.css -->
+        <!-- ============================================================== -->
+        <div id="preloader"><div class="preloader"><span></span><span></span></div></div>
+		
+		
+        <!-- ============================================================== -->
+        <!-- Main wrapper - style you can find in pages.scss -->
+        <!-- ============================================================== -->
+        <div id="main-wrapper">
