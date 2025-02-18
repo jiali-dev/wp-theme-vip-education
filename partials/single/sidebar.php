ffd54eb4f4f3307cb2c19bef983004d85d1f5f48
jiali <mahyarerad@gmail.com>
2025-02-18 15:55:56 +0330

    Creating sidebar parts

--- partials/single/sidebar.php
+++ partials/single/sidebar.php
@@ -7,4 +7,12 @@ if (!defined('ABSPATH')) exit;
 
 <!-- Single blog Grid -->
 <div class="col-lg-4 col-md-12 col-sm-12 col-12">
+    <!-- Searchbard -->
+    <?php get_template_part( 'partials/single/_sidebar-searchbard', 'searchbard' ) ?>
+    <!-- Categories -->
+    <?php get_template_part( 'partials/single/_sidebar-categories', 'categories' ) ?>
+    <!-- Trending Posts -->
+    <?php get_template_part( 'partials/single/_sidebar-trending-posts', 'trending-posts' ) ?>
+    <!-- Tags -->
+    <?php get_template_part( 'partials/single/_sidebar-tags', 'tags' ) ?>
 </div>
