46b1de8fa58bef46ec8831a8d1f25e7cce5a9ce3
jiali <mahyarerad@gmail.com>
2025-02-18 15:57:05 +0330

    Creating single parts

--- single.php
+++ single.php
@@ -10,6 +10,16 @@ if (!defined('ABSPATH')) exit;
 
 <!-- Navbar -->
 <?php get_template_part( 'partials/nav/nav', 'nav' ) ?>
+<!-- Page Title -->
+<?php get_template_part( 'partials/single/title', 'title' ) ?>
+<!-- Content Layout Start -->
+<?php get_template_part( 'partials/single/content-layout-start', 'content-layout-start' ) ?>
+<!-- Main Content -->
+<?php get_template_part( 'partials/single/main-content', 'main-content' ) ?>
+<!-- Sidebar -->
+<?php get_template_part( 'partials/single/sidebar', 'sidebar' ) ?>
+<!-- Content Layout End -->
+<?php get_template_part( 'partials/single/content-layout-end', 'content-layout-end' ) ?>
 
 <!-- Footer -->
 <?php get_footer(  ); ?>
