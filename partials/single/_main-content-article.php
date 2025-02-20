a71bb83a4c1a4f4e0d7f4f9696ff92673cd4f6e0
jiali <mahyarerad@gmail.com>
2025-02-20 15:37:39 +0330

    Modifying Main content article by new template parts

--- partials/single/_main-content-article.php
+++ partials/single/_main-content-article.php
@@ -13,12 +13,9 @@ if (!defined('ABSPATH')) exit;
             <img class="img-fluid" src="<?php echo VIP_EDUCATION_IMAGES_URI . '/h-5.png' ?>" alt="">
         </div>
         
-        <div class="article_top_info">
-            <ul class="article_middle_info">
-                <li><a href="#"><span class="icons"><i class="ti-user"></i></span>توسط الهام کریمی</a></li>
-                <li><a href="#"><span class="icons"><i class="ti-comment-alt"></i></span>45 نظر ثبت شده</a></li>
-            </ul>
-        </div>
+        <!-- Post Meta -->
+        <?php get_template_part( 'partials/meta-data/single/post-meta', 'post-meta' ) ?>
+
         <h2 class="post-title">کنکور هم با همه بالا و پایین هاش بالاخره تمام شد...</h2>
         <p>کنکور99 هم با حضور یک میلیون و چهارصد هزار شرکت کننده به پایان رسید و هر آنچه که دیده شد اضطراب و نگرانی داوطلبان از شیوع بیماری کرونا و و تاثیر آن بر سلامت روان خانواده ها و داوطلبان کنکوری بود.
             امید داریم کنکور 1400 در فضایی به دور از هرگونه بیماری و با آرامش بیشتری برگزار شود و خاطره تلخ این روزها را با خودش ببرد.
@@ -33,24 +30,12 @@ if (!defined('ABSPATH')) exit;
             <br><br>کنکور در ایران همیشه دغدغه دانش آموزان بوده و هست. پیشتر, از سال های اول دبیرستان دانش آموزان وارد این چالش می شدند و 4 سال با آن درگیر بودند. هر سال یک بیانه صادر می شد و نحوه محاسبه تراز و رتبه اعلام می شد اما باز همانی برگزار می شد که بود. اما امروزه از دوران ابتدایی خانواده فرزندان خود را درگیر کنکور می کنند و با نوشتن کلاس های مختلف دانش آموزان را برای این برهه از زندگی آماده می کنند و به خیال خودشان کار درستی است اما اگر واقع بین باشیم این امر بسیار مخرب است. جدا از سیستم آموزشی که به هر حال اینطور رقم خورده, فرزندانمان نیاز به کسب مهرات هم دارند. مهارت ها و هنرهایی که چه بسا می توانند بیشتر کارآیی داشته باشند.
             کافیست به این درک برسیم که دانشگاه رفتن برای کسب سواد اجتماعی و شعور اجتماعی لازم است اما بدون مدرک دانشگاهی هم می توان زندگی کرد و می توان بسیار هم موفق بود.</p>
         <div class="article_bottom_info">
-            <div class="post-tags">
-                <h4 class="pbm-title">تگ های پربازدید</h4>
-                <ul class="list">
-                    <li><a href="#">کنکور</a></li>
-                    <li><a href="#">موفقیت</a></li>
-                    <li><a href="#">تدریس</a></li>
-                </ul>
-            </div>
-            <div class="post-share">
-                <h4 class="pbm-title">شبکه های اجتماعی</h4>
-                <ul class="list">
-                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                    <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
-                    <li><a href="#"><i class="fab fa-vk"></i></a></li>
-                    <li><a href="#"><i class="fab fa-tumblr"></i></a></li>
-                </ul>
-            </div>
+            <!-- Post Tags -->
+            <?php get_template_part( 'partials/meta-data/single/post-tags', 'post-tags' ) ?>
+
+            <!-- Post Share -->
+            <?php get_template_part( 'partials/meta-data/single/post-share', 'post-share' ) ?>
+
         </div>
         <div class="single_article_pagination">
             <div class="prev-post">
