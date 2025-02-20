8e94a841b75fd57939e370c759845bbd40d4002e
jiali <mahyarerad@gmail.com>
2025-02-20 15:36:54 +0330

    Post tags part

---
+++ partials/meta-data/single/post-tags.php
@@ -0,0 +1,17 @@
+<?php 
+
+// Exit if accessed directly
+if (!defined('ABSPATH')) exit;
+
+?>
+
+<!-- Post Tags -->
+
+<div class="post-tags">
+    <h4 class="pbm-title">تگ های پربازدید</h4>
+    <ul class="list">
+        <li><a href="#">کنکور</a></li>
+        <li><a href="#">موفقیت</a></li>
+        <li><a href="#">تدریس</a></li>
+    </ul>
+</div>
