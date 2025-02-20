23ff6551760f3721f92a958b32dc9b7827f1b738
jiali <mahyarerad@gmail.com>
2025-02-20 14:09:05 +0330

    Content Layout Start and End

---
+++ partials/archive/content-layout-start.php
@@ -0,0 +1,10 @@
+<?php 
+
+// Exit if accessed directly
+if (!defined('ABSPATH')) exit;
+
+?>
+
+<!-- ============================ Full Width Courses  ================================== -->
+<section class="pt-0">
+				<div class="container">
