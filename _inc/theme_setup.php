d4e956e29918215c6ca52d65033fd0f485c8068b
jiali <mahyarerad@gmail.com>
2025-02-21 15:18:13 +0330

    Register Top menu in them setup

--- _inc/theme_setup.php
+++ _inc/theme_setup.php
@@ -6,5 +6,8 @@ if (!defined('ABSPATH')) exit;
 function vip_education_theme_setup() {
     // Hide admin bar
     add_filter( 'show_admin_bar', '__return_false' );
+    
+    // Register menu
+    register_nav_menu( 'Top Menu', 'منو برای بالای سایت' );
 }
 add_action( 'after_setup_theme', 'vip_education_theme_setup');
