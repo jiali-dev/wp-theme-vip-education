6a44b6b72c8067313e5450cac4ce6e0fe91ece5b
jiali <mahyarerad@gmail.com>
2025-02-21 22:22:06 +0330

    Making menu as wp menu and more dynamically

--- _inc/theme_setup.php
+++ _inc/theme_setup.php
@@ -8,6 +8,9 @@ function vip_education_theme_setup() {
     add_filter( 'show_admin_bar', '__return_false' );
     
     // Register menu
-    register_nav_menu( 'Top Menu', 'منو برای بالای سایت' );
+    register_nav_menu( 'Primary', 'منو برای بالای سایت' );
+
+    // Register Bootstap Navwalker
+    require_once( VIP_EDUCATION_CLASS_PATH . '/WP_Bootstrap_Navwalker.php');
 }
 add_action( 'after_setup_theme', 'vip_education_theme_setup');
