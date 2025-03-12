593567d36f4325bc17034186d8d1b05b9e74be14
jiali <mahyarerad@gmail.com>
2025-03-12 16:44:25 +0330

    Making loop for tech post

--- partials/index/tech-posts.php
+++ partials/index/tech-posts.php
@@ -18,56 +18,7 @@ if (!defined('ABSPATH')) exit;
         
         <div class="row">
                     
-            <!-- Single Article -->
-            <div class="col-lg-4 col-md-4 col-sm-12">
-                <div class="articles_grid_style">
-                    <div class="articles_grid_thumb">
-                        <a href="blog-detail.html"><img src="<?php echo VIP_EDUCATION_IMAGES_URI . '/b-1.jpg' ?>' ?>" class="img-fluid" alt="" /></a>
-                    </div>
-                    
-                    <div class="articles_grid_caption">
-                        <h4>چطور زبان انگلیسی را سریع یاد بگیریم؟</h4>
-                        <div class="articles_grid_author">
-                            <div class="articles_grid_author_img"><img src="<?php echo VIP_EDUCATION_IMAGES_URI . '/user-1.jpg' ?>" class="img-fluid" alt="" /></div>
-                            <h4>آدام ویلسون</h4>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            
-            <!-- Single Article -->
-            <div class="col-lg-4 col-md-4 col-sm-12">
-                <div class="articles_grid_style">
-                    <div class="articles_grid_thumb">
-                        <a href="blog-detail.html"><img src="<?php echo VIP_EDUCATION_IMAGES_URI . '/b-2.jpg' ?>" class="img-fluid" alt="" /></a>
-                    </div>
-                    
-                    <div class="articles_grid_caption">
-                        <h4>لزوم یادگیری زبان در دنیای امروزی!</h4>
-                        <div class="articles_grid_author">
-                            <div class="articles_grid_author_img"><img src="<?php echo VIP_EDUCATION_IMAGES_URI . '/user-2.jpg' ?>" class="img-fluid" alt="" /></div>
-                            <h4>جان اسمیت</h4>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            
-            <!-- Single Article -->
-            <div class="col-lg-4 col-md-4 col-sm-12">
-                <div class="articles_grid_style">
-                    <div class="articles_grid_thumb">
-                        <a href="blog-detail.html"><img src="<?php echo VIP_EDUCATION_IMAGES_URI . '/b-3.jpg' ?>" class="img-fluid" alt="" /></a>
-                    </div>
-                    
-                    <div class="articles_grid_caption">
-                        <h4>چطور امتحان حضوری موفقی داشته باشیم؟</h4>
-                        <div class="articles_grid_author">
-                            <div class="articles_grid_author_img"><img src="<?php echo VIP_EDUCATION_IMAGES_URI . '/user-3.jpg' ?>" class="img-fluid" alt="" /></div>
-                            <h4>دنیل ویکیونز</h4>
-                        </div>
-                    </div>
-                </div>
-            </div>
+           <?php get_template_part( 'loop/technology-loop', 'technology-loop' ) ?>
         
         </div>
     </div>
