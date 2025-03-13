89c502bf4016c260ecc83cd9310c4bbbb4a7c070
jiali <mahyarerad@gmail.com>
2025-03-14 02:23:25 +0330

    Adding function for make text shorter

--- _inc/post/post.php
+++ _inc/post/post.php
@@ -6,3 +6,7 @@ if (!defined('ABSPATH')) exit;
 function jve_default_post_thumbnail() {
     return '<img class="img-responsive" src="https://hackr.io/blog/what-is-programming-language/thumbnail/large" alt="image">';
 }
+
+function jve_make_content_shorter( $content ) {
+    return mb_substr( $content, 0, 100) . ' ...';
+}
