c5a3247749eda5e330f9586b39af7046732a84c5
jiali <mahyarerad@gmail.com>
2025-03-14 02:22:56 +0330

    Modify archive page

--- archive.php
+++ archive.php
@@ -9,7 +9,6 @@ if (!defined('ABSPATH')) exit;
 
 <!-- Header -->
 <?php get_header(  );?>
-
 <!-- Navbar -->
 <?php get_template_part( 'partials/nav/nav', 'nav' ) ?>
 <!-- Page Title -->
@@ -22,6 +21,5 @@ if (!defined('ABSPATH')) exit;
 <?php get_template_part( 'partials/archive/main-content', 'main-content' ) ?>
 <!-- Content Layout End -->
 <?php get_template_part( 'partials/archive/content-layout-end', 'content-layout-end' ) ?>
-
 <!-- Footer -->
 <?php get_footer(  ); ?>
