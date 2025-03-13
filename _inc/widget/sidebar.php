f38fc32b67374392c419e27b57d5f8ff76acd1d9
jiali <mahyarerad@gmail.com>
2025-03-13 15:45:05 +0330

    Dynamic sidebar due to post type

--- _inc/widget/sidebar.php
+++ _inc/widget/sidebar.php
@@ -4,10 +4,22 @@
 if (!defined('ABSPATH')) exit;
 
 function vip_education_sidebar_init() {
+    // Single post sidebar
     register_sidebar([
-        'name'          => 'سایدبار پست ها',
-        'id'            => 'single-sidebar',
+        'name'          => 'سایدبار صفحه پست',
+        'id'            => 'single-post-sidebar',
         'description'   => 'یک سایدبار اختصاصی برای صفحه پست ها',
+        'before_widget' => '<div class="widget %2$s">', // It wraps each widget
+        'after_widget'  => '</div>',
+        'before_title'  => '<h3 class="widget-title">',
+        'after_title'   => '</h3>',
+    ]);
+
+    // Single technology sidebar
+    register_sidebar([
+        'name'          => 'سایدبار صفحه تکنولوژی',
+        'id'            => 'single-technology-sidebar',
+        'description'   => 'یک سایدبار اختصاصی برای صفحه تکنولوژی ها',
         'before_widget' => '<div class="widget %2$s">', // It wraps each widget
         'after_widget'  => '</div>',
         'before_title'  => '<h3 class="widget-title">',
--- sidebar-single.php
+++ sidebar-single.php
@@ -4,6 +4,6 @@
 if (!defined('ABSPATH')) exit;
 
 // Define sidebar
-if (is_active_sidebar('single-sidebar')) {
-    dynamic_sidebar('single-sidebar');
+if (is_active_sidebar('single-'.get_post_type(  ).'-sidebar')) {
+    dynamic_sidebar('single-'.get_post_type(  ).'-sidebar');
 }
