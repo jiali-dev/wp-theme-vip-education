b54b18160da843881ca78a6b9f4ab60aa90dc259
jiali <mahyarerad@gmail.com>
2025-03-23 17:28:12 +0330

    Creating contact page UI

---
+++ partials/contact/main-content.php
@@ -0,0 +1,135 @@
+<?php 
+
+// Exit if accessed directly
+if (!defined('ABSPATH')) exit;
+
+?>
+
+<!-- ============================ Agency List Start ================================== -->
+<section class="bg-light">
+			
+    <div class="container">
+    
+        <!-- row Start -->
+        <div class="row">
+        
+            <div class="col-lg-8 col-md-7">
+                <form class="contact-form">
+                    <div class="prc_wrap">
+                        
+                        <div class="prc_wrap_header">
+                            <h4 class="property_block_title">فرم تماس</h4>
+                        </div>
+                        
+                        <div class="prc_wrap-body">
+                            <div class="row">
+                                <div class="col-lg-6 col-md-12">
+                                    <div class="form-group">
+                                        <label>نام کامل</label>
+                                        <input type="text" name="fullname" class="form-control simple">
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-md-12">
+                                    <div class="form-group">
+                                        <label>ایمیل</label>
+                                        <input type="email" name="email" class="form-control simple">
+                                    </div>
+                                </div>
+                            </div>
+                            
+                            <div class="form-group">
+                                <label>عنوان</label>
+                                <input type="text" name="title" class="form-control simple">
+                            </div>
+                            
+                            <div class="form-group">
+                                <label>پیام</label>
+                                <textarea name="message" class="form-control simple"></textarea>
+                            </div>
+                            
+                            <div class="form-group">
+                                <button class="btn btn-theme" type="submit">ارسال درخواست</button>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </form> 
+                <div class="response"></div>            
+            </div>
+            
+            <div class="col-lg-4 col-md-5">
+            
+                <div class="prc_wrap">
+                    
+                    <div class="prc_wrap_header">
+                        <h4 class="property_block_title">راه های ارتباطی</h4>
+                    </div>
+                    
+                    <div class="prc_wrap-body">
+                        <div class="contact-info">
+                    
+                            <h2>در تماس باشید</h2>
+                            <p>برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. </p>
+                            
+                            <div class="cn-info-detail">
+                                <div class="cn-info-icon">
+                                    <i class="ti-home"></i>
+                                </div>
+                                <div class="cn-info-content">
+                                    <h4 class="cn-info-title">آدرس ما</h4>
+                                    ایران، تهران <br>خیابان سعادت آباد،<br> خیابان کاج شمال غربی
+                                </div>
+                            </div>
+                            
+                            <div class="cn-info-detail">
+                                <div class="cn-info-icon">
+                                    <i class="ti-email"></i>
+                                </div>
+                                <div class="cn-info-content">
+                                    <h4 class="cn-info-title">پست الکترونیکی</h4>
+                                    support@example.com<br>example@gmail.com
+                                </div>
+                            </div>
+                            
+                            <div class="cn-info-detail">
+                                <div class="cn-info-icon">
+                                    <i class="ti-mobile"></i>
+                                </div>
+                                <div class="cn-info-content">
+                                    <h4 class="cn-info-title">شماره تماس</h4>
+                                    (41) 123 521 458<br>+91 235 548 7548
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+            
+        </div>
+        <!-- /row -->		
+        
+    </div>
+            
+</section>
+<!-- ============================ Agency List End ================================== -->
+
+<!-- ============================== Start Newsletter ================================== -->
+<section class="newsletter theme-bg inverse-theme">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-7 col-md-8 col-sm-12">
+                <div class="text-center">
+                    <h2>به جامعه هزاران دانشجو بپیوندید!</h2>
+                    <p>به جامعه میلیونی دانشجویان ما بپیوندید و به هزاران ساعت آموزش در حوزه‌های گوناگون دسترسی داشته باشید.</p>
+                    <form class="sup-form">
+                        <input type="email" class="form-control sigmup-me" placeholder="ایمیل خود را وارد کنید..." required="required">
+                        <input type="submit" class="btn btn-theme" value="عضویت">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- ================================= End Newsletter =============================== -->
